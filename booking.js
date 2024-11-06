258c811afd99751d32bb0d570be509f83f26f51c
groovyhooked <dafrenchie2002@yahoo.fr>
2024-11-06 16:50:01 +0100

    Minor modifications

--- booking.js
+++ booking.js
@@ -1,28 +1,32 @@
 
-const panier = document.querySelector('.panier')
+const panier = document.querySelector('.trajetGlobal')
 
 
 fetch('http://localhost:3000/booking')
-.then(Response => Response.json())
-.then(data => {
-    for(let i = 0; i < data[i]; i++) {
-    let bookedElement = bookHtmlTemplate(data[i]);
-    panier.innerHTML += bookedElement;
-    } 
-})
+    .then(Response => Response.json())
+    .then(data => {
+        console.log(data.booking);
+        if (data.booking.length > 0) {
+            panier.innerHTML = ''
+            for (let i = 0; i < data.booking.length; i++) {
+                console.log('test');
+
+                let bookedElement = bookHtmlTemplate(data.booking[i]);
+                panier.innerHTML += bookedElement;
+            }
+        }
+    })
 
 
 const bookHtmlTemplate = data => {
+    console.log('DEBUG', data.departure);
+
     const date = new Date(data.date)
     const hours = date.getHours()
     let minutes = date.getMinutes()
     minutes = minutes.toString().length === 1 ? `0${minutes}` : minutes
-    
+
     return `
-<div class="middlebox">
-        <div class="panier">
-            <div class="text">My bookings</div>
-            <div class="trajetGlobal">
                 <div class="row-result">
                     <div class="deparr">
                         <div id="departure">${data.departure}</div>
@@ -35,11 +39,5 @@ const bookHtmlTemplate = data => {
                         <div class="decompte">Departure in 5 hours</div>
                     </div>
                 </div>
-            </div>
-            <div class="messageResa">
-                <div id="Remerciements">Enjoy your travel with Tickethack </div>
-            </div>
-        </div>
-    </div>
     `
 }
